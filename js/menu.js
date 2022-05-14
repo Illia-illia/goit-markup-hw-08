b96959589e2faf32091b6e7082934305cedfff3c
Illia-Illia <Takoexerrorx@gmail.com>
2022-05-14 11:04:46 +0300

    add mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
-    const MenuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-
-    MenuBtnRef.addEventListener("click", () => {
-        const expanded =
-            MenuBtnRef.getAttribute("aria-expanded") === "true" || false;
-        
-        MenuBtnRef.classList.toggle("is-open");
-        MenuBtnRef.setAttribute("aria-expanded", !expanded);
-
-        mobileMenuRef.classList.toggle("is-open");
-    });
+  const refs = {
+    openMenuBtn: document.querySelector(".menu__button"),
+    closeMenuBtn: document.querySelector(".mob-menu__button"),
+    menu: document.querySelector(".mob-menu"),
+    body: document.querySelector(".body"),
+  };
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
 })();
